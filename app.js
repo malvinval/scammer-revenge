3c21696b64e7676c4027a33dabbff43a8be2f317
malvinval <malvinval09@gmail.com>
2024-02-15 09:59:45 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,122 @@
+const { default: axios } = require("axios");
+
+const token = "bot6861974788:AAH_AbVplVpEG3XTRhVIgbMVqIeT-_y7vPQ";
+const message = "LO%20PENIPU%20KAN!!!!"
+const chatId = "6951548817";
+
+const url = `https://api.telegram.org/${token}/sendMessage?parse_mode=markdown&chat_id=${chatId}&text=${message}`
+
+async function sendMessage() {
+    try {
+        while (true) {
+            await axios.get(url)
+                .then((response) => {
+                    if (response.status == 200) {
+                        console.log(response.statusText)
+                    } else {
+                        console.log("message failed!")
+                    }
+                })
+        }
+
+    } catch (e) {
+        sendMessage();
+    }
+}
+
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+
