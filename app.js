8a3035507c7a2aa58d671ce88d589ae281da6dc6
malvinval <malvinval09@gmail.com>
2024-02-24 14:39:19 +0700

    update: code

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-const { default: axios } = require("axios");
+const {default: axios} = require("axios")
 
-const token = "bot6861974788:AAH_AbVplVpEG3XTRhVIgbMVqIeT-_y7vPQ";
-const message = "LO%20PENIPU%20KAN!!!!"
-const chatId = "6951548817";
-
-const url = `https://api.telegram.org/${token}/sendMessage?parse_mode=markdown&chat_id=${chatId}&text=${message}`
+const url = "https://api.telegram.org/bot6861974788:AAH_AbVplVpEG3XTRhVIgbMVqIeT-_y7vPQ/sendMessage?parse_mode=markdown&chat_id=6951548817&text=test"
 
 async function sendMessage() {
     try {
@@ -12,14 +8,12 @@ async function sendMessage() {
             await axios.get(url)
                 .then((response) => {
                     if (response.status == 200) {
-                        console.log(response.statusText)
-                    } else {
-                        console.log("message failed!")
+                        console.log(response)
                     }
                 })
         }
-
-    } catch (e) {
+    } catch (error) {
+        console.log("API down!")
         sendMessage();
     }
 }
@@ -55,68 +49,3 @@ sendMessage();
 sendMessage();
 sendMessage();
 sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-sendMessage();
-
