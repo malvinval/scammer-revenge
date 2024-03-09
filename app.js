553ce3ec62fc263208dae7dd6edeea8e4fb05887
NatsumeAoii <149252113+NatsumeAoii@users.noreply.github.com>
2024-03-10 01:24:25 +0700

    Update app.js
    add message counter

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const axios = require('axios');
+
+let totalMessagesSent = 0;
+
 async function sendMessages(urls, customText) {
   try {
     console.log(`Sending: "${customText}"`);
@@ -14,16 +17,16 @@ async function sendMessages(urls, customText) {
           }
         })
         .catch((error) => {
-          return `Link ${index + 1}: API down! ${error.message}`;
+          return `Link ${index + 1}: API down!: ${error.message}`;
         })
     );
-
     const results = await Promise.allSettled(requests);
     results.forEach((result) => {
       console.log(result.value);
     });
-
-    console.log('='.repeat(30));
+    totalMessagesSent++;
+    console.log(`Total messages sent: ${totalMessagesSent}`);
+    console.log('='.repeat(30));    
   } catch (error) {
     console.error('Error:', error.message);
   }
@@ -31,12 +34,13 @@ async function sendMessages(urls, customText) {
 
 async function main() {
   try {
+    const urls = fs
+      .readFileSync('urls.txt', 'utf8')
+      .trim()
+      .split('\n');
+    const customText = 'Bismillah Mati'; // Custom text
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     while (true) {
-      const urls = fs
-        .readFileSync('urls.txt', 'utf8')
-        .trim()
-        .split('\n');
-      const customText = 'Bismillah Mati'; //ADD YOUR CUSTOM TEXT HERE
       await sendMessages(urls, customText);
     }
   } catch (error) {
